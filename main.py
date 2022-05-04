8d8d157d6f8498e4f17fcc9b5d8b3efd49a8aafd
acezack <kal.lal@hotmail.com>
2022-05-04 22:31:11 +0200

    okay, it may work or may not, depends on what algorithm so far, idiot

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
-from time import sleep
+"""
+Additional libraries needed to run:
+    pip install pillow
+    pip install opencv-contrib-python
+    pip install opencv-python
+"""
 
+from time import sleep
 import cv2
 import os
 import pickle
 import numpy as np
 from PIL import Image
 
+gray = 0
 label_ids = []
 x_train, y_labels = 0, 0
 recognizer = 0
-face_cascade = cv2.CascadeClassifier(
-    'C:\\Users\\adjakobs\\Desktop\\test\\venv\\lib\\site-packages\\cv2\\data\\haarcascade_frontalface_alt2.xml')
 
 
-def detect(cam):
-    cap = cv2.VideoCapture(cam)
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
 
-    while (True):
+# Since we are testing different algorithms
+def algorithm():
+    global recognizer
+    print("Choose algorithm: 'f' for Fisherface, 'e' for Eigenface, or 'l' for LBPH:")
+    type = ""
+    while type != 'f' and type != 'e' and type != 'l':
+        type = input()
+        if type == 'f':
+            recognizer = cv2.face.FisherFaceRecognizer_create()
+        elif type == 'e':
+            recognizer = cv2.face.EigenFaceRecognizer_create()
+        elif type == 'l':
+            recognizer = cv2.face.LBPHFaceRecognizer_create()
+        else:
+            print("Unknown operation, please enter 'f' for Fisherface, 'e' for Eigenface, or 'l' for LBPH:")
+
+
+def detect(cam_in):
+    cap = cv2.VideoCapture(cam_in)
+
+    while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
+        global gray
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, minNeighbors=5)  # scaleFactor
 
@@ -29,8 +52,6 @@ def detect(cam):
         for (x, y, w, h) in faces:
             roi_gray = gray[y:y + h, x:x + w]
             roi_color = frame[y:y + h, x:x + w]
-            # img_item = 'my_image.png'
-            # cv2.imwrite(img_item, roi_gray)
 
             # Draw a rectangle around detected faces
             color = (255, 0, 0)  # BGR (opencv default)
@@ -51,9 +72,65 @@ def detect(cam):
     cv2.destroyAllWindows()
 
 
+def generate(cam_in):
+    cap = cv2.VideoCapture(cam_in)
+    retval, image = cap.read()
+    global gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    print("Name of subject? ")
+    name = input()
+
+    if not os.path.isdir('./images/' + name):
+        os.mkdir('./images/' + name)
+    else:
+        print("Subject name already exists. Enter 'a' to add new images of subject, or 'q' to return.")
+        cont = False
+        while not cont:
+            op = input()
+            if op == 'a':
+                cont = True
+            elif op == 'q':
+                return
+            else:
+                print("Unknown operation.")
+
+    print("How many test images? ")
+    number = int(input())
+    print("How much time between each image (in seconds, e.g. '0.5')?")
+    time = float(input())
+    print("Capturing image in: 3", end="")
+    sleep(1)
+    print(", 2", end="")
+    sleep(1)
+    print(", 1")
+    sleep(1)
+    for i in range(number):
+        retval, image = cap.read()
+        fileindex = i
+        file = "./images/" + name + "/" + name + "_" + str(fileindex) + ".png"
+
+        while os.path.isfile(file):
+            fileindex += 1
+            file = "./images/" + name + "/" + name + "_" + str(fileindex) + ".png"
+            # print(file, os.path.isfile(file))
+        cv2.imwrite(file, image)
+        cv2.imshow('frame', image)
+        # Press q to quit/turn off camera
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+        if number > 1:
+            sleep(time)
+        print("\rImage {} of {} taken.".format(i + 1, number), end="")
+    print("\nData generated.")
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def train():
     # Define image directory
-    image_dir = "C:\\Users\\adjakobs\\Desktop\\test\\images"
+    image_dir = "./images"
 
     # Initialize training set and labels
     global x_train, y_labels, label_ids
@@ -69,20 +146,29 @@ def train():
     global gray
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # print(gray)
-    index = 0
+
+    subjects = [f.name for f in os.scandir(image_dir) if f.is_dir()]
+    print("Found {} subjects: ".format(len(subjects)), end="")
+    cleaned = ""
+    for subject in subjects:
+        cleaned = cleaned + subject + ", "
+    cleaned = cleaned[0:len(cleaned) - 2] + ".\nCommencing training..."
+    print(cleaned, end="")
     for root, dirs, files in os.walk(image_dir):
+
+        index = 0
+        subject = os.path.basename(root)
         for file in files:
+            index = index + 1
+            print("\rTraining on subject '{}' is {:.0%} complete.".format(subject, index / len(files)), end="")
             if file.endswith('png') or file.endswith('jpg') or file.endswith('jpeg'):
                 path = os.path.join(root, file)
-                label = os.path.basename(root)  # os.path.dirname(path)
-                # path = path.replace('\\', '/')
-                # print(label, path)
+                label = os.path.basename(root)
 
                 if label not in label_ids:
                     label_ids[label] = current_id
                     current_id += 1
                 id_ = label_ids[label]
-                print(current_id)
 
                 pil_image = Image.open(path).convert('L')
                 # Resize image (if necessary)
@@ -92,20 +178,22 @@ def train():
                 image_array = np.array(final_image, 'uint8')  # change pil_image to final_image if you resize the image
 
                 gray = cv2.cvtColor(cv2.resize(cv2.imread(path), size), cv2.COLOR_BGR2GRAY)
-
+                #print(len(gray))
                 faces = face_cascade.detectMultiScale(gray, minNeighbors=5)
+
+                cv2.PCA
 
                 # Detect faces in the image
                 for (x, y, w, h) in faces:
+                    print(w,h)
                     roi = image_array[y:y + h, x:x + w]
                     x_train.append(roi)
                     y_labels.append(id_)
-        index = index + 1
+        print()
     if y_labels != [] and x_train != []:
-        print(y_labels)
-        print(x_train)
+        print("Training complete.\n")
     else:
-        print("shit's fucked")
+        print("\nshit's fucked, ABORT, ABORT\n")
 
     recognizer.train(x_train, np.array(y_labels))
     recognizer.save("trainer.yml")
@@ -115,26 +203,9 @@ def train():
         pickle.dump(label_ids, f)
 
 
-def algorithm():
-    global recognizer
-    print("Choose method. 'f' for Fisherface, 'e' for Eigenface, or 'l' for LBPH:")
-    type = input()
-    if type == 'f':
-        recognizer = cv2.face.FisherFaceRecognizer_create()
-    elif type == 'e':
-        recognizer = cv2.face.EigenFaceRecognizer_create()
-    elif type == 'l':
-        recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-
-def recognise(cam):
+def recognise(cam_in):
     recognizer.read('trainer.yml')
 
-    # Inverse label
-    # label = {v: k for k, v in label_ids.items()}
-    # for index in label:
-    #    print(label[index])
-    # print(label[0], label[1], label[2])
     label = {}
     with(open('labels.pickle', 'rb')) as openfile:
         try:
@@ -144,32 +215,33 @@ def recognise(cam):
         except EOFError:
             ()
 
-    for index in label:
-        print(label[index])
-    print(label)
+    accuracy = {}
     # Initialize web cam
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(cam_in)
 
     # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    print("Below the average conf will be displayed in this format: ('subject' : 'average conf' : 'current conf')")
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
         global gray
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, minNeighbors=5)
-        print(faces)
+
         for (x, y, w, h) in faces:
             roi_gray = gray[y:y + h, x:x + w]
             roi_color = frame[y:y + h, x:x + w]
             id_, conf = recognizer.predict(roi_gray)
 
-            # img_item = 'my_image.png'
-            # cv2.imwrite(img_item, roi_gray)
-            print(conf)
+            name = label[id_]
+            try:
+                accuracy[name]
+            except KeyError:
+                accuracy[name] = [conf]
+
+            accuracy[name].append(conf)
             if conf >= 45:
-                print(id_)
-                print(label[id_])
 
                 # Draw labels on the Haar Cascade rectangle
                 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -185,12 +257,13 @@ def recognise(cam):
             end_cord_y = y + h
             cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
-            ## Optional: create rectangles for eyes
-            # = eye_cascade.detectMultiScale(roi_gray)
-            # for (ex, ey, ew, eh) in eyes:
-            #    cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
+            out = ""
+            for subject in accuracy:
+                out = out + "({} : {:.0f} : {:.0f}), ".format(subject,
+                                                              np.mean(accuracy[subject]),
+                                                              accuracy[subject][-1])
+            print("\r" + out[0:len(out) - 2], end="")
 
-        # Display the resulting frame
         cv2.imshow('frame', frame)
 
         # Press q to quit/turn off camera
@@ -200,69 +273,52 @@ def recognise(cam):
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
+    print()
 
 
-def generate(cam, name, number):
-    cap = cv2.VideoCapture(cam)
-    retval, image = cap.read()
-    global gray
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    print("Capturing image in: 3", end="")
-    sleep(0.5)
-    print(", 2", end="")
-    sleep(0.5)
-    print(", 1")
-    sleep(0.5)
-
-    if not os.path.isdir('./images/' + name):
-        os.mkdir('./images/' + name)
-    for i in range(number):
-        retval, image = cap.read()
-        sleep(0.2)
-
-        fileindex = i
-        file = "./images/" + name + "/" + name + "_" + str(fileindex) + ".png"
-
-        while os.path.isfile(file):
-            fileindex = fileindex + 1
-            file = "./images/" + name + "/" + name + "_" + str(fileindex) + ".png"
-            print(file, os.path.isfile(file))
-        cv2.imwrite(file, image)
-    print("Data generated.")
-    # print(image)
+def display_help():
+    print("\n'g' will generate training data. You will be asked to input the name of the subject, how many "
+          "pictures to capture, and the delay between each picture (measured in seconds).")
+    print("'t' will use the previously generated data to train the model upon. This will go through and train "
+          "on each picture.")
+    print("'d' is only used to see if the algorithm will detect a face at all.")
+    print("'r' will use the live input from a camera combined with the trained model and try to label each "
+          "face in the live feed with the corresponding label from the model.")
+    print("'h' is how you got here.")
+    print("'q' to quit. Does this need further explanation?\n")
 
 
 if __name__ == "__main__":
     algorithm()
-
     print("Choose camera '0', or '1':")
-    cam = int(input())
-    print("Center your face in front of camera.")
-    retval, image = cv2.VideoCapture(cam).read()
-    print(image)
-    print()
-    global gray
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    cam = ""
+    while isinstance(cam, str):
+        try:
+            cam = int(input())
+        except ValueError:
+            print("Only enter a singular number. E.g. '0', '1', or '22'.")
+
     done = False
     while not done:
-        print("Enter 'g', to generate, 't' to train, 'd' to detect, 'r' to recognise, or 'q' to quit:")
+        print("Enter 'g', to generate, 't' to train, 'd' to detect, 'r' to recognise, 'h' for additional info,"
+              " or 'q' to quit:")
         op = input()
         if op == 'g':
             print("This will generate training data.")
-            print("Name of subject? ")
-            name = input()
-            print("How many test images? ")
-            number = int(input())
-            generate(cam, name, number)
+            generate(cam)
         elif op == 't':
             print("This will train the model on the training data.")
             train()
         elif op == 'd':
             print("This will show detected faces.")
+            print("Press 'q' while the window is focused to exit.")
             detect(cam)
         elif op == 'r':
             print("This will recognise previously learned subjects.")
             recognise(cam)
+        elif op == 'h':
+            display_help()
         elif op == 'q':
             done = True
         else:
